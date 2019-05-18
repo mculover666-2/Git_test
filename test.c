93ea0a032b7e328eaf6eaae9f32e9d9c23d55b17
mculover666 <2412828003@qq.com>
2019-05-18 13:55:16 +0800

    mculover666-2加入Hello函数

--- test.c
+++ test.c
@@ -1,8 +1,20 @@
 #include <stdio.h>
 
+/**
+ * @ author mculover666-2
+ * */
+int hello()
+{
+    printf("Hello.\n");
+
+    return 0;
+}
+
 int main(void)
 {
     printf("Hello World.\n");
+
+    hello();
 
     return 0;
 }
