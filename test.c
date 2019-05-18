7219af7a0d8b36f59d6fc3558084a719b5c49152
mculover666-2 <1768391279@qq.com>
2019-05-18 14:16:40 +0800

    mculover666 add function hello

--- test.c
+++ test.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
+/**
+ * @author mculover666-2
+ * */
+int hello()
+{
+    printf("Hello.\n");
+    return 0;
+}
+
 int main(void)
 {
     printf("Hello World.\n");
+
+    hello();
 
     return 0;
 }
