2cdc3ef0b978cae659592186922c9f404aa23714
mculover666-2 <1768391279@qq.com>
2019-05-20 11:06:16 +0800

    mculover666-2 add function goodbye

--- test.c
+++ test.c
@@ -8,12 +8,22 @@ int hello()
     printf("Hello.\n");
     return 0;
 }
+/**
+ * @ author mculover666-2
+ * */
+int goodbye()
+{
+    printf("Goodbye.\n");
+    return 0;
+}
 
 int main(void)
 {
     printf("Hello World.\n");
 
     hello();
+
+    goodbye();
 
     return 0;
 }
